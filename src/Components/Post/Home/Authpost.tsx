2b1c02051cee923991e21da56addebb4e339fc92
asainr <96766868+asainr@users.noreply.github.com>
2022-01-02 22:50:18 -0800

    Update Authpost.tsx

--- src/Components/Post/Home/Authpost.tsx
+++ src/Components/Post/Home/Authpost.tsx
@@ -3,11 +3,13 @@ import axios, { AxiosResponse } from "axios";
 import { Button, Container, Row, Col } from "react-bootstrap";
 import Head from "../../Headers/header";
 import { Link } from "react-router-dom";
+import { ExportCSV } from "../Excel/ExportCsv";
 
 const Authpost = () => {
-  const [UseData, setUseData] = useState<any>([])
-  const [filter, setfilter] = useState<any>([])
-  const [listDatafilter, setListDatafilter] = useState<any>([])
+  const [UseData, setUseData] = useState<any>([]);
+  const [filter, setfilter] = useState<any>([]);
+  const [listDatafilter, setListDatafilter] = useState<any>([]);
+  const fileName = 'Go Rest API'
 
   useEffect(() => {
     axios
@@ -38,30 +40,30 @@ const Authpost = () => {
           .get(`https://gorest.co.in/public/v1/users/${element.user_id}`)
           .then((response: any) => {
             let res = response.data.data;
-            let dres =delete element.id;
+            let dres = delete element.id;
             finalData.push({ ...element, ...res });
             const timer = setTimeout(() => {
-              let arrdd:any[] = finalData
-              setListDatafilter([...arrdd])
+              let arrdd: any[] = finalData;
+              setListDatafilter([...arrdd]);
             }, 1000);
             return () => clearTimeout(timer);
           });
       });
     }
-    
   }, [UseData]);
 
-  const dataSearch = 
-  listDatafilter.length &&
-  listDatafilter.filter((data:any) => {
-  return Object.keys(data).some(key => 
-    data[key].toString().toLowerCase().includes(filter.toString().toLowerCase())
-    )
-  });
-  
-  console.log(dataSearch);
-  
+  const dataSearch =
+    listDatafilter.length &&
+    listDatafilter.filter((data: any) => {
+      return Object.keys(data).some((key) =>
+        data[key]
+          .toString()
+          .toLowerCase()
+          .includes(filter.toString().toLowerCase())
+      );
+    });
 
+  console.log(dataSearch);
 
   return (
     <div className="App">
@@ -76,27 +78,35 @@ const Authpost = () => {
                     Home Page with Posts and Author Name
                   </h2>
                 </div>
-                <label className="form-label h4">Search</label>{' '}
+                <label className="form-label h4">Search</label>{" "}
                 <input
                   type="text"
                   className="from-control"
                   onChange={(e) => setfilter(e.target.value)}
                 />
+                <div>
+                <ExportCSV  csvData={dataSearch} fileName={fileName} />
+              </div>
               </div>
             </div>
-            {dataSearch && dataSearch.length && dataSearch.map((data: any, index: any) => {
+            {dataSearch &&
+              dataSearch.length &&
+              dataSearch.map((data: any, index: any) => {
                 return (
                   <Col sm={4} md={6} xl={6} style={{ marginBottom: "10px" }}>
                     {" "}
-                    <div className="card h-100" style={{border:'2px solid green'}}>
-            <Link to={`/user/${data?.id}`}>
-              <h1 className="card-header">{data?.name}</h1>
-            </Link>
-            <h4 className="card-header">{data.title}</h4>
-            <div className="card-body">
-              <p className="card-text">{data.body}</p>
-            </div>
-          </div>
+                    <div
+                      className="card h-100"
+                      style={{ border: "2px solid green" }}
+                    >
+                      <Link to={`/user/${data.id}`}>
+                        <h1 className="card-header">{data.name}</h1>
+                      </Link>
+                      <h4 className="card-header">{data.title}</h4>
+                      <div className="card-body">
+                        <p className="card-text">{data.body}</p>
+                      </div>
+                    </div>
                   </Col>
                 );
               })}
@@ -162,3 +172,4 @@ const Authpost = () => {
   );
 };
 export default Authpost;
+
